df6b0e632e95a20b11c70c31278c5ceb32611d60
Laurynas Remeika <laurynas@inventi.lt>
2017-04-01 21:13:27 +0300

    Authenticate using google service

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login';
+import { Button } from 'antd';
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: null
+    };
+    this.responseGoogle = this.responseGoogle.bind(this);
+  }
+
+
+   responseGoogle(e) {
+      this.setState({user: e.profileObj.givenName})
+      console.log(e);
+    };
+
+  renderLogin() {
+    if (this.state.user) {
+      return (<span> Logged in: {this.state.user}</span>)
+    } else {
+      return (
+        <GoogleLogin
+          className="ant-btn ant-btn-primary"
+          clientId={'658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com'}
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+          offline={false} />
+      );
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {this.renderLogin()}
       </div>
     );
   }
